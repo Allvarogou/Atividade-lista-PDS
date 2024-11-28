27bba7f02cdeb4553f4952dc9a663212830e2b58
Alvaro <165071441+Allvarogou@users.noreply.github.com>
2024-11-28 10:38:53 -0300

    Atividade 28/11

---
+++ MavenFXNumeros/src/main/java/controller/ControllerTela2.java
@@ -0,0 +1,100 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class ControllerTela2 {
+@FXML
+    private RadioButton C;
+
+    @FXML
+    private CheckBox GP;
+
+    @FXML
+    private Button Linux;
+
+    @FXML
+    private Button Mac;
+
+    @FXML
+    private RadioButton Python;
+
+    @FXML
+    private Button Sub;
+
+    @FXML
+    private RadioButton java;
+
+    @FXML
+    private Label lblLing;
+
+    @FXML
+    private Label lblNome;
+
+    @FXML
+    private Label lblSo;
+
+    @FXML
+    private TextField nome;
+
+    @FXML
+    private CheckBox progalld;
+
+    @FXML
+    private Button window;
+
+    @FXML
+    void OnClickBtnSub(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickBtnlinux(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickBtnmac(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickBtnwdw(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickCbGP(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickCbprogd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickRdC(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickRdPython(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickRdjava(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClicktxtnome(ActionEvent event) {
+
+    }
+}
